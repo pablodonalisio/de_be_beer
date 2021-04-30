bb40ca518b6f52b42a724304347ec8712540015a
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-04-30 12:05:49 -0300

    Create ProductData.js

---
+++ src/ProductData.js
@@ -0,0 +1,34 @@
+export const ProductData = [
+  {
+    itemID: 1,
+    name: "Cerveza IPA",
+    brand: "Andes",
+    image:
+      "https://gamadistribuidora.com.ar/301-thickbox_default/andes-origen-ipa-ret-x12-1000.jpg",
+    price: "240,00",
+  },
+  {
+    itemID: 2,
+    name: "Cerveza Rubia",
+    brand: "Andes",
+    image:
+      "http://maxerfoods.com/wp-content/uploads/2021/01/cerveza-andes-rubia-1.jpg",
+    price: "200,00",
+  },
+  {
+    itemID: 3,
+    name: "Cerveza Roja",
+    brand: "Andes",
+    image:
+      "https://tudeliveryonline.com.ar/tienda/wp-content/uploads/2020/08/roja.jpg",
+    price: "220,00",
+  },
+  {
+    itemID: 4,
+    name: "Cerveza Negra",
+    brand: "Andes",
+    image:
+      "https://www.supereldesafio.com.ar/wp-content/uploads/2020/09/1674-ANDES-CERVEZA-NEGRA-1000-CM.jpg",
+    price: "220,00",
+  },
+];
