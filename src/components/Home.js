b227a8be09b32cb09ccde6d1625806ceda3e29c8
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-04-30 11:27:22 -0300

    Add bg-color

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,9 @@ const Home = () => {
     <div>
       <div className="sticker">
         <p className="sticker-text">
-          Disfruta de las mejores juntadas con amigos. Nosotros te llevamos la
-          bebida!
+          Disfruta de las mejores juntadas con amigos.
+          <br />
+          Nosotros te llevamos la bebida!
         </p>
         <img
           src="https://www.infocampo.com.ar/wp-content/uploads/2019/04/cerveza-argentina.jpg"
